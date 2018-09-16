e53a1efd03f4beae7e02799872f2d68efb0ede08
Argenis <kchangch@sfu.ca>
2018-09-16 07:06:50 -0700

    Added format on homepage

--- containers/HomeContainer.js
+++ containers/HomeContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Dimensions, Text, TextInput, Image } from "react-native"
+import { View, StyleSheet, ScrollView, Dimensions, Text, TextInput, Image, ImageBackground, Button, Alert} from "react-native"
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class HomeContainer extends Component {
@@ -14,30 +14,59 @@ export default class HomeContainer extends Component {
         super(props)
 
         this.state = {
-            text: ''
+            text: 'a'
         }
     }
 
+    testing() {
+            // Alert.alert(this.state.text)
+        console.warn(this.state.text)
+    }
+
     render() {
+        console.warn(this.state.text)
         return <View style={styles.container}>
-            <View style={styles.videoContainer}>
-                <Image
-                    source={require('../videos/demo.gif')}
-                    style={{width: 300, height: 300}}
-                />
-            </View>
-            <ScrollView 
-                contentContainerStyle={styles.questionContainer}
+            <ImageBackground source = {require('../Assets/background.jpg')}
+                style = {styles.backgroundContainer}
             >
-                <Text style={{fontSize: 15}}>Hi there! What are your thoughts on this situation?</Text>
-                <View>
-                    <TextInput
-                        style={{height: 100, width: winWidth - 20, borderWidth: 1, borderColor: 'transparent'}}
-                        onChangeText={(text) => this.setState({text})}
-                        maxLength = {200}
-                    />          
+                <View style={styles.videoContainer}>
+                    <Image
+                        source={require('../videos/demo.gif')}
+                        style={styles.imageContainer}
+                    />
                 </View>
-            </ScrollView>
+                <ScrollView 
+                    contentContainerStyle={styles.questionContainer}
+                >
+                    <View style = {styles.gameQuestionContainer}>
+                        <Text style={{fontSize: 15, fontWeight: 'bold'}}>Hi there! What are your thoughts on this situation?</Text>
+                           
+                           <View style = {{paddingTop: 20}}>
+                            <TextInput
+                                multiline={true}
+                                style={styles.textContainer}
+                                onChangeText={(text) => this.setState({text})}
+                                maxLength = {200}
+                            />
+                            </View>
+
+                            <View>
+                                <Button
+                                  onPress={this.testing}
+                                  title="Submit"
+                                  color="#841584"
+                                  accessibilityLabel="Learn more about this purple button"
+                                />
+
+                            </View>
+
+                    </View>
+                </ScrollView>
+
+                </ImageBackground>
+                
+
+
         </View>
     }
 }
@@ -48,22 +77,49 @@ const winWidth = Dimensions.get('window').width
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'transparent',
     },
     videoContainer: {
-        backgroundColor: 'white',
+        backgroundColor: 'transparent',
+        borderColor: '#03A9F4',
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height/2,
         justifyContent: 'center',
         alignItems: 'center',
     },
     questionContainer: {
-        backgroundColor: '#03A9F4',
+        backgroundColor: 'transparent',
         flex: 1,
         width: Dimensions.get('window').width,
         height: 100,
         alignItems: 'center',
         paddingTop: 20,
+        borderRadius: 20,
     },
+    imageContainer: {
+        borderRadius: 30, 
+        width: (Dimensions.get('window').width + 100) / 2,
+        justifyContent: 'center',
+    },
+    gameQuestionContainer: {
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textContainer: {
+        backgroundColor: '#ffca28',
+        height: 100, 
+        width: winWidth - 30, 
+        borderWidth: 10, 
+        borderColor: 'transparent',
+        borderRadius: 20,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+    },
+    backgroundContainer: {
+        width: winWidth,
+        height: winHeight,
+    }
 })
 
