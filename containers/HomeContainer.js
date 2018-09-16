2b0c630670c09d8d7a17fc7194e53458cf08136c
lijason103 <lijason103@gmail.com>
2018-09-15 22:52:26 -0700

    Added gif

--- containers/HomeContainer.js
+++ containers/HomeContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { View } from "react-native"
-import { HeaderBackButton } from 'react-navigation'
+import { View, StyleSheet, ScrollView, Dimensions, Text, TextInput, Image } from "react-native"
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class HomeContainer extends Component {
@@ -11,10 +10,58 @@ export default class HomeContainer extends Component {
     }
    
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            text: ''
+        }
+    }
+
     render() {
-        return <View style={{flex: 1, backgroundColor: 'red'}}>
-  
+        return <View style={styles.container}>
+            <View style={styles.videoContainer}>
+                <Image
+                    source={require('../videos/demo.gif')}
+                    style={{width: 300, height: 300}}
+                />
+            </View>
+            <ScrollView 
+                contentContainerStyle={styles.questionContainer}
+            >
+                <Text style={{fontSize: 15}}>Hi there! What are your thoughts on this situation?</Text>
+                <TextInput
+                    style={{height: 100, width: winWidth - 20, borderWidth: 1, borderColor: 'transparent'}}
+                    onChangeText={(text) => this.setState({text})}
+                    maxLength = {200}
+                />
+            </ScrollView>
         </View>
     }
 }
+
+const winHeight = Dimensions.get('window').height
+const winWidth = Dimensions.get('window').width
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    videoContainer: {
+        backgroundColor: 'white',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height/2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    questionContainer: {
+        backgroundColor: '#03A9F4',
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: 100,
+        alignItems: 'center',
+        paddingTop: 20,
+    },
+})
 
