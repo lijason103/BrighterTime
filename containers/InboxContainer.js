995a171d1667b43e5a1e794c958dc5161f8a52e8
lijason103 <lijason103@gmail.com>
2018-09-15 21:01:43 -0700

    Added inbox

--- containers/InboxContainer.js
+++ containers/InboxContainer.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import { View } from "react-native"
+import { StyleSheet, View, ScrollView, Text } from "react-native"
 
 export default class InboxContainer extends Component {
 
-    render() {
-        return <View style={{flex: 1, backgroundColor: 'blue'}}>
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: 'Inbox',
+        }
+    }
 
+    render() {
+        return <View style={styles.container}>
+            <ScrollView
+                style={styles.scrollContainer}
+            >
+                {list.map((value, index) => {
+                    return <View 
+                        key={index}
+                        style={styles.card}>
+                        <Text>{value}</Text>
+                    </View>
+                })
+
+                }
+            </ScrollView>
         </View>
     }
 }
 
+const list = ['a','b','c','d','e','f']
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scrollContainer: {
+        flex: 1,
+    },
+    card: {
+        backgroundColor: 'white', 
+        height: 100,
+        flex: 1,
+        marginBottom: 10,
+    }
+})
